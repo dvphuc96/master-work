fb1daef78f795e0510c8330b32ab3b1a402022ab
dvphuc96 <phuc.dovan.96@gmail.com>
2022-10-14 11:20:51 +0700

    add end file

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 function openModal() {
-    document.querySelector("#videoModal").style.display = "block";
-  }
-  function closeModal() {
-    document.querySelector("#videoModal").style.display = "none";
-  }
+  document.querySelector("#videoModal").style.display = "block";
+}
+function closeModal() {
+  document.querySelector("#videoModal").style.display = "none";
+}
